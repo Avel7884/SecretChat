6d1f4839a5c219424db934e5fe4ae445eeff2bc8
khanova <khanovaanna1@gmail.com>
2018-06-08 11:14:21 +0300

    Added crypto streams

---
+++ VKApi/CryptoStream.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace VKApi
+{
+    public class CryptoStream : Stream
+    {
+        private Stream underlayingStream;
+        private KeyReader keyReader;
+
+        public CryptoStream(Stream underlayingStream, KeyReader keyReader)
+        {
+            this.underlayingStream = underlayingStream;
+            this.keyReader = keyReader;
+        }
+        
+        public override void Flush()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            var read = underlayingStream.Read(buffer, offset, count);
+            var partOfKey = new byte[count];
+            keyReader.Read(partOfKey, 0, read);
+            for (var i = offset; i < read + offset; ++i)
+                buffer[i] = (byte) (partOfKey[i - offset] ^ buffer[i]);
+            return read;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            var toWrite = new byte[count];
+            var partOfKey = new byte[count];
+            keyReader.Read(partOfKey, 0, count);
+            for (var i = offset; i < count + offset; ++i)
+                toWrite[i - offset] = (byte) (partOfKey[i - offset] ^ buffer[i]);
+            underlayingStream.Write(toWrite, 0, count);
+        }
+
+        public override bool CanRead { get; }
+        public override bool CanSeek { get; }
+        public override bool CanWrite { get; }
+        public override long Length { get; }
+        public override long Position { get; set; }
+    }
+}
---
+++ VKApi/KeyReader.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+
+namespace VKApi
+{
+    public class KeyReader : Stream, IDisposable
+    {
+        private string pathToKey;
+        private StreamReader keyReader;
+        private bool isDisposed;
+        
+        public KeyReader(string pathToKey)
+        {
+            isDisposed = false;
+            this.pathToKey = pathToKey;
+            keyReader = new StreamReader(pathToKey);
+        }
+
+        public void Dispose()
+        {
+             Dispose(true);   
+        }
+
+        protected virtual void Dispose(bool fromDisposedMethod)
+        {
+            if (isDisposed) return;
+            isDisposed = true;
+            keyReader.Close();
+        }
+        
+        public override void Flush()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            var temporaryBuffer = new char[count];
+            readExactlyCount(temporaryBuffer, count);
+
+            for (var i = offset; i < count + offset; ++i)
+                buffer[i] = (byte) temporaryBuffer[i - offset];
+            return count;
+        }
+
+        private void readExactlyCount(char[] buffer, int count)
+        {
+            var readed = keyReader.Read(buffer, 0, count);
+            while (readed != count)
+            {
+                keyReader.Close();
+                keyReader = new StreamReader(pathToKey);
+                readed += keyReader.Read(buffer, readed, count - readed);
+            }
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool CanRead { get; }
+        public override bool CanSeek { get; }
+        public override bool CanWrite { get; }
+        public override long Length { get; }
+        public override long Position { get; set; }
+    }
+}
